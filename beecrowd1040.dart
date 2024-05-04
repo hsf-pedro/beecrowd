6fb305f02b9d710b60e523fbe8b6c1189f0b5702
Pedro Ferreira <pedro-@live.jp>
2024-05-04 15:55:31 -0300

    fix: minor problems

--- beecrowd1040.dart
+++ beecrowd1040.dart
@@ -1,36 +1,28 @@
 import "dart:io";
 
 void main() {
-  var notas = stdin.readLineSync()!.split(" ");
-  var nota1 = double.parse(notas[0]);
-  var nota2 = double.parse(notas[1]);
-  var nota3 = double.parse(notas[2]);
-  var nota4 = double.parse(notas[3]);
+  var nota = stdin.readLineSync()!.split(" ");
+  var nota1 = double.parse(nota[0]);
+  var nota2 = double.parse(nota[1]);
+  var nota3 = double.parse(nota[2]);
+  var nota4 = double.parse(nota[3]);
+  var media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 4) + (nota4 * 1)) / 10;
+  print('Media: ${media.toStringAsFixed(1)}');
 
-  var media =
-      ((nota1 * 2.0) + (nota2 * 3.0) + (nota3 * 4.0) + (nota4 * 1.0)) / 10.0;
-  bool aprovado = (media >= 7.00000);
-  bool reprovado = (media < 5.00000);
-  bool exame = (media >= 5.00000) && (media <= 6.90000);
-
-  if (aprovado) {
-    print('Media: ${media.toStringAsFixed(1)}');
+  if (media >= 7.0) {
     print('Aluno aprovado.');
-  } else if (reprovado) {
-    print('Media: ${media.toStringAsFixed(1)}');
+  } else if (media < 5.0) {
     print('Aluno reprovado.');
-  } else if (exame) {
-    print('Media: ${media.toStringAsFixed(1)}');
+  } else {
     print('Aluno em exame.');
     var notaExame = double.parse(stdin.readLineSync()!);
     print('Nota do exame: ${notaExame.toStringAsFixed(1)}');
-    var notaFinal = ((media + notaExame) / 2);
-    bool notaF1 = ((notaFinal >= 5.00000));
-    bool notaF2 = ((notaFinal <= 4.90000));
-    if (notaF1) {
+    var mediaFinal = (media + notaExame) / 2;
+    if (mediaFinal >= 5.0) {
       print('Aluno aprovado.');
-    } else if (notaF2) {
+    } else {
       print('Aluno reprovado.');
     }
+    print('Media final: ${mediaFinal}');
   }
 }
